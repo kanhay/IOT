65d05b17186c7103e4d86c2bebd071cbb7ee064e
kanhay <woshidija@gmail.com>
2026-07-02 21:02:19 +0100

    part 2 is working as expected

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -2,9 +2,9 @@
 set -eu
 
 IP="$1"
-IFACE=$(ip -4 -o addr show | awk -v ip="$IP" '$4 ~ ip {print $2}')
+IFACE=$(ip -4 -o addr show | awk -v ip="$IP" '$4 ~ ip {print $2}') #Finding the interface
 
-export DEBIAN_FRONTEND=noninteractive
+# export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install -y curl
 
@@ -15,7 +15,7 @@ curl -sfL https://get.k3s.io | sh -s - server \
 
 mkdir -p /home/vagrant/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
-sed -i "s/127.0.0.1/$IP/" /home/vagrant/.kube/config
+sed -i "s/127.0.0.1/$IP/" /home/vagrant/.kube/config #Replacing localhost
 chown -R vagrant:vagrant /home/vagrant/.kube
 echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
 
@@ -23,7 +23,7 @@ echo "export KUBECONFIG=/home/vagrant/.kube/config" >> /home/vagrant/.bashrc
 mkdir -p /var/lib/rancher/k3s/server/manifests
 cp /vagrant/confs/*.yaml /var/lib/rancher/k3s/server/manifests/
 
-echo "Waiting for node to be Ready..."
+echo "Waiting for node to become Ready..."
 until kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml get nodes | grep -q " Ready"; do
-  sleep 2
+    sleep 2
 done
